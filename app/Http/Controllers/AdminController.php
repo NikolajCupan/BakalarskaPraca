4544a14903f0bba091c072fa1ab47eab812f4710
cupan1 <cupan1@stud.uniza.sk>
2023-02-09 21:07:27 +0100

    Disallow logged user access to login, register page

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class AdminController extends Controller
+{
+    public static function admin()
+    {
+        if (!Gate::allows('administrate'))
+        {
+            abort(403);
+        }
+
+        return view('user.admin');
+    }
+}
