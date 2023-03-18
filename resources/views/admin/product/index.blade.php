70180144cf7b05a068d68180da82832e857699e7
cupan1 <cupan1@stud.uniza.sk>
2023-03-18 12:26:08 +0100

    Products management description added

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+    <link rel="stylesheet" href="{{asset('css/adminStyles.css')}}">
+
     <x-other.flashMessage/>
     <x-navbar.navbarAdmin homePath="/admin/product"/>
 
@@ -11,7 +13,18 @@
                 <x-menu.categoryMenuAdminProduct/>
             </div>
             <div class="col-md-12 col-lg-9">
-                Nothing
+                <h3 class="title">Sprava produktov</h3>
+                <p class="mb-0">Nasledujuca sekcia sluzi na spravovanie produktov. Produkty su rozdelene do 2 kategorii:</p>
+                <ul>
+                    <li><strong>Sklad</strong> - Predstavuje produkt nachadzajuci sa na sklade. Skladovany produkt ma nazov a pocet skladovanych kusov. Nazov musi byt unikatny, pocet skladovanych kusov mozno menit.</li>
+                    <li><strong>Obchod</strong> - Po spusteni predaja skladovaneho tovaru sa z neho stava produkt v obchode. Predavany produkt ma cenu, kategoriu, fotku a popis. Vsetky tieto polozky mozno pocas predaja menit.</li>
+                </ul>
+
+                <p>Jeden skladovany produkt moze byt asociovany s viacerymi predavanymi produktmi, ale v jednom case moze byt skladovany produkt predavany maximalne raz.
+                                <strong>Priklad:</strong> skladovany produkt sa predaval od 01.02.2023 do 31.05.2023 a dalsi predaj zacal od 01.06.2023. Pri prvom predaji bol prepojeny
+                                s inym predavanym produktom ako pri druhom. A v case ked sa nepredaval nebol prepojeny so ziadnym.</p>
+
+                <p>Predavany produkt si pamata historiu cien pocas svojho predaja a objednavky, v ktorych sa nachadza.</p>
             </div>
         </div>
     </div>
--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -84,3 +84,7 @@
     </div>
 
 @endsection
+
+@section('footer')
+    <x-footer.footer/>
+@endsection
